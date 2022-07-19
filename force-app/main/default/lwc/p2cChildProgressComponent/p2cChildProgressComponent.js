bd0d1af0f4111a8a93fe4f34f0abd2595578d80f
Dhiraj <dhirajw27@gmail.com>
2022-07-19 15:53:09 +0530

    second commit

---
+++ force-app/main/default/lwc/p2cChildProgressComponent/p2cChildProgressComponent.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class P2cChildProgressComponent extends LightningElement {
+    @api progressValue
+}
--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,28 +1,26 @@
 import { LightningElement } from 'lwc';
 
 export default class P2cParentComponent extends LightningElement {
-    carouselData = [
+    carouselData=[
         {
             src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
             header:"First Card",
-            description:"First Description",
-            altText:"First Card Accessible Description",
-            link:"https://www.google.com"
+            description:"First Description"
         },
         {
             src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
             header:"Second Card",
-            description:"Second Description",
-            altText:"Second Card Accessible Description",
-            link:"https://www.google.com"
+            description:"Second Description"
         },
         {
             src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
             header:"Third Card",
-            description:"Third Description",
-            altText:"Third Card Accessible Description",
-            link:"https://www.google.com"
+            description:"Third Description"
         }
     ]
-}
 
+    percentage=10
+    handlechange(event){
+        this.percentage = event.target.value
+    }
+}
